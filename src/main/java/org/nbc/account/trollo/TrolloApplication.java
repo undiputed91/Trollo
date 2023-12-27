d01258f08346824916d29f4a1eb290a8348d03f4
phdljr <phdljr@naver.com>
2023-12-27 17:17:50 +0900

    init project

---
+++ src/main/java/org/nbc/account/trollo/TrolloApplication.java
@@ -0,0 +1,13 @@
+package org.nbc.account.trollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrolloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nbc/account/trollo/TrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nbc.account.trollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrolloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
