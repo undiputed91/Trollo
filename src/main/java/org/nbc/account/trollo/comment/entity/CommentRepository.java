9b0d9df7869b09ee762ae43d8b297c36a22deb65
pangseon <chaosen0521@gmail.com>
2023-12-28 18:20:23 +0900

    [#33]Fix: Comment @Repository 삭제

--- src/main/java/org/nbc/account/trollo/comment/entity/CommentRepository.java
+++ src/main/java/org/nbc/account/trollo/comment/entity/CommentRepository.java
@@ -1,10 +1,7 @@
 package org.nbc.account.trollo.comment.entity;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CommentRepository extends JpaRepository <CommentEntity, Long> {
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
 }
