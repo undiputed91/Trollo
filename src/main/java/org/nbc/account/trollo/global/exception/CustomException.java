900119be227928dfe863ebb345a013031f5b14b4
phdljr <phdljr@naver.com>
2023-12-28 15:32:50 +0900

    [#49]Feat: 커스텀 예외 설정

---
+++ src/main/java/org/nbc/account/trollo/global/exception/CustomException.java
@@ -0,0 +1,11 @@
+package org.nbc.account.trollo.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
