468804dfd13978603effc3c8d08ac2e767131580
Minsun <minisundev@gmail.com>
2023-12-28 11:48:41 +0900

    [#38]Feat: UserDetails 구현

---
+++ src/main/java/org/nbc/account/trollo/global/security/UserDetailsImpl.java
@@ -0,0 +1,53 @@
+package org.nbc.account.trollo.global.security;
+
+import java.util.Collection;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.nbc.account.trollo.domain.user.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+  @Getter
+  private final User user;
+
+  public UserDetailsImpl(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+
+}
---
+++ src/main/java/org/nbc/account/trollo/global/security/UserDetailsService.java
@@ -0,0 +1,21 @@
+package org.nbc.account.trollo.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.nbc.account.trollo.domain.user.entity.User;
+import org.nbc.account.trollo.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public UserDetailsImpl getUserDetails(String email) {
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException(email+" is not found"));
+    return new UserDetailsImpl(user);
+  }
+
+}
