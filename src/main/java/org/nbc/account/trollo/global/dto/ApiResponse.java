6a2cc9bdacc0ecd8939ef255ebaba0e37bc51dc8
Minsun <minisundev@gmail.com>
2023-12-28 11:51:27 +0900

    [#40]Feat: 전역 response,에러처리 설정

---
+++ src/main/java/org/nbc/account/trollo/global/dto/ApiResponse.java
@@ -0,0 +1,29 @@
+package org.nbc.account.trollo.global.dto;
+
+import lombok.Getter;
+
+@Getter
+
+public class ApiResponse<T> {
+
+  private int status;
+  private String message;
+  private T data;
+
+  public ApiResponse(int status, String message, T data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
+
+  public ApiResponse(int status, String message) {
+    this.status = status;
+    this.message = message;
+    this.data = null;
+  }
+
+  public ApiResponse(T data) {
+    this.data = data;
+  }
+
+}
