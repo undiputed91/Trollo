190b5be2ade434c8f56a526ef9e4562a4842c701
pangseon <chaosen0521@gmail.com>
2023-12-29 13:34:32 +0900

    [#32]Feat: Comment 생성시 닉네임 추가

--- src/main/java/org/nbc/account/trollo/domain/comment/dto/res/CommentSaveRes.java
+++ src/main/java/org/nbc/account/trollo/domain/comment/dto/res/CommentSaveRes.java
@@ -1,8 +1,10 @@
-package org.nbc.account.trollo.comment.dto.res;
+package org.nbc.account.trollo.domain.comment.dto.res;
 
 import jakarta.validation.constraints.Size;
 
 public record CommentSaveRes(
+    @Size(max = 500)
+    String nickname,
     @Size(max = 500)
     String content
 ) {
