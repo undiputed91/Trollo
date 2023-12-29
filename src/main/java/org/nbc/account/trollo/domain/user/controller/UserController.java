7894c58ea0c66fa81768a4f868e127e493a01e65
Minsun <minisundev@gmail.com>
2023-12-29 14:40:02 +0900

    Chore: 쓰지 않는 annotation 제거

--- src/main/java/org/nbc/account/trollo/domain/user/controller/UserController.java
+++ src/main/java/org/nbc/account/trollo/domain/user/controller/UserController.java
@@ -3,7 +3,6 @@ package org.nbc.account.trollo.domain.user.controller;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.nbc.account.trollo.domain.user.dto.request.LoginReq;
 import org.nbc.account.trollo.domain.user.dto.request.SignupReq;
 import org.nbc.account.trollo.domain.user.service.UserService;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
