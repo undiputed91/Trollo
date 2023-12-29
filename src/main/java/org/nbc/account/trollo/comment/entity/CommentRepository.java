21766b41ba5880de7a4be738a7a175f487f39ac2
pangseon <chaosen0521@gmail.com>
2023-12-29 13:30:32 +0900

    [#32]Rename: Comment 도메인 위치 수정

--- src/main/java/org/nbc/account/trollo/comment/entity/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.nbc.account.trollo.comment.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-
-}
