6b0287af9cf3d510c6b0fb7de07e435e5412bb73
seunghosin <shin2896@naver.com>
2023-12-31 16:47:21 +0900

    [#19]Refactor:카드서비스 주입x 새 메서드

--- src/main/java/org/nbc/account/trollo/domain/checklist/service/impl/CheckListServiceImpl.java
+++ src/main/java/org/nbc/account/trollo/domain/checklist/service/impl/CheckListServiceImpl.java
@@ -5,13 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.nbc.account.trollo.domain.card.entity.Card;
 import org.nbc.account.trollo.domain.card.exception.NotFoundCardException;
 import org.nbc.account.trollo.domain.card.repository.CardRepository;
-import org.nbc.account.trollo.domain.card.service.CardService;
 import org.nbc.account.trollo.domain.checklist.dto.request.CheckListRequestDto;
 import org.nbc.account.trollo.domain.checklist.entity.CheckList;
 import org.nbc.account.trollo.domain.checklist.exception.NotFoundCheckListException;
 import org.nbc.account.trollo.domain.checklist.repository.CheckListRepository;
 import org.nbc.account.trollo.domain.checklist.service.CheckListService;
 import org.nbc.account.trollo.domain.user.entity.User;
+import org.nbc.account.trollo.domain.userboard.entity.UserBoard;
+import org.nbc.account.trollo.domain.userboard.entity.UserBoardRole;
+import org.nbc.account.trollo.domain.userboard.exception.NotFoundUserBoardException;
+import org.nbc.account.trollo.domain.userboard.repository.UserBoardRepository;
 import org.nbc.account.trollo.global.exception.ErrorCode;
 import org.nbc.account.trollo.global.security.UserDetailsImpl;
 import org.springframework.stereotype.Service;
@@ -22,7 +25,7 @@ public class CheckListServiceImpl implements CheckListService {
 
     private final CardRepository cardRepository;
     private final CheckListRepository checkListRepository;
-    private final CardService cardService;
+    private final UserBoardRepository userBoardRepository;
 
     @Override
     public void createList(Long cardId, CheckListRequestDto requestDto,
@@ -36,7 +39,7 @@ public class CheckListServiceImpl implements CheckListService {
 
         Long boardId = card.getSection().getBoard().getId();
 
-        cardService.checkUserInBoard(boardId, loginUser.getId());
+        checkUserInBoard(boardId, loginUser.getId());
 
         CheckList checkList = CheckList.builder()
             .card(card)
@@ -61,7 +64,7 @@ public class CheckListServiceImpl implements CheckListService {
             .orElseThrow(() -> new NotFoundCheckListException(ErrorCode.NOT_FOUND_CHECKLIST));
 
         Long boardId = card.getSection().getBoard().getId();
-        cardService.checkUserInBoard(boardId, loginUser.getId());
+        checkUserInBoard(boardId, loginUser.getId());
 
         checkList.update(description, checkSign);
     }
@@ -76,9 +79,16 @@ public class CheckListServiceImpl implements CheckListService {
             .orElseThrow(() -> new NotFoundCheckListException(ErrorCode.NOT_FOUND_CHECKLIST));
 
         Long boardId = card.getSection().getBoard().getId();
-        cardService.checkUserInBoard(boardId, loginUser.getId());
+        checkUserInBoard(boardId, loginUser.getId());
 
         checkListRepository.delete(checkList);
     }
 
+    private void checkUserInBoard(Long boardId, Long userId) {
+        UserBoard userBoard = userBoardRepository.findByBoardIdAndUserId(boardId, userId)
+            .orElseThrow(() -> new NotFoundUserBoardException(ErrorCode.NOT_FOUND_USER_BOARD));
+        if (userBoard.getRole() == UserBoardRole.WAITING) {
+            throw new ForbiddenAccessBoardException(ErrorCode.FORBIDDEN_ACCESS_BOARD);
+        }
+    }
 }
--- src/main/java/org/nbc/account/trollo/domain/userboard/repository/UserBoardRepository.java
+++ src/main/java/org/nbc/account/trollo/domain/userboard/repository/UserBoardRepository.java
@@ -1,5 +1,6 @@
 package org.nbc.account.trollo.domain.userboard.repository;
 
+import java.util.Optional;
 import org.nbc.account.trollo.domain.userboard.entity.UserBoard;
 import org.nbc.account.trollo.domain.userboard.entity.UserBoardId;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserBoardRepository extends JpaRepository<UserBoard, UserBoardId> {
 
     boolean existsByBoardIdAndUserId(Long boardId, Long userId);
+
+    Optional<UserBoard> findByBoardIdAndUserId(Long boardId, Long userId);
 }
