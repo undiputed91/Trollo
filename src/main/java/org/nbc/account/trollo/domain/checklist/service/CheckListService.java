d91555f28576c321526de2c6a7e0cb6f7d524abc
seunghosin <shin2896@naver.com>
2024-01-02 20:56:22 +0900

    Refactor: 생성할 때 true값 설정 제거 (수정으로만 true 가능하게 변경)

--- src/main/java/org/nbc/account/trollo/domain/checklist/service/CheckListService.java
+++ src/main/java/org/nbc/account/trollo/domain/checklist/service/CheckListService.java
@@ -1,11 +1,12 @@
 package org.nbc.account.trollo.domain.checklist.service;
 
+import org.nbc.account.trollo.domain.checklist.dto.request.CheckListCreateRequestDto;
 import org.nbc.account.trollo.domain.checklist.dto.request.CheckListRequestDto;
 import org.nbc.account.trollo.global.security.UserDetailsImpl;
 
 public interface CheckListService {
 
-    void createList(Long cardId, CheckListRequestDto requestDto, UserDetailsImpl userDetails);
+    void createList(Long cardId, CheckListCreateRequestDto requestDto, UserDetailsImpl userDetails);
 
     void updateCheckList(Long cardId, Long checkListId, CheckListRequestDto requestDto,
         UserDetailsImpl userDetails);
